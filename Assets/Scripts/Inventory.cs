d7d151c47906a712f97d93a18dbe11b4a3875e42
12Cy <christian.sandkaemper@gmail.com>
2016-03-12 20:56:45 +0100

    switch items

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -25,6 +25,7 @@ public class Inventory : MonoBehaviour {
         {//--Erstellt die Slots!
             items.Add(new Item());
             slots.Add(Instantiate(inventorySlot));//BlankSlotCreate
+            slots[i].GetComponent<Slot>().id = i;
             slots[i].transform.SetParent(slotPanel.transform); //Position durch VererbungGesetzt
         }
 
@@ -42,21 +43,55 @@ public class Inventory : MonoBehaviour {
     public void AddItem(int id)
     {
         Item itemToAdd = database.FetchItemByID(id); //Sucht das Item per ID raus
-        for (int i = 0; i < items.Count; i++)
+        if (itemToAdd.Stackable && CheckIfItemIsInInventory(itemToAdd)) //Schaut nach ob ein Item Stackbar ist
         {
-            //EmptyItem
-            if (items[i].ID == -1) //--Wenn die ID gefunden wurde ->
+            for (int i = 0; i < items.Count; i++)
             {
-                items[i] = itemToAdd;
-                GameObject itemObj = Instantiate(inventoryItem); //item korrekter Slot
-                itemObj.transform.SetParent(slots[i].transform);
-
-                itemObj.transform.position = new Vector2(5,-5); //###############Position des Sprites
-                itemObj.GetComponent<Image>().sprite = itemToAdd.Sprite;
-                itemObj.name = itemToAdd.Title;
-
-                break; //ABbrechen, nachdems gefunden wurde
+                if (items[i].ID == id)
+                {
+                    ItemData data = slots[i].transform.GetChild(0).GetComponent<ItemData>();
+                    data.amount++;
+                    data.transform.GetChild(0).GetComponent<Text>().text = data.amount.ToString(); //Gibt die Anzahl als Text für das Child             }
+                    break;
+                }
             }
         }
+        else
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                //EmptyItem
+                if (items[i].ID == -1) //--Wenn die ID gefunden wurde ->
+                {
+                    items[i] = itemToAdd;
+                    GameObject itemObj = Instantiate(inventoryItem); //item korrekter Slot
+                    itemObj.GetComponent<ItemData>().item = itemToAdd;
+                    itemObj.GetComponent<ItemData>().slot = i;
+                    itemObj.transform.SetParent(slots[i].transform);
+
+                    itemObj.transform.position = new Vector2(5, -5); //###############Position des Sprites
+                    itemObj.GetComponent<Image>().sprite = itemToAdd.Sprite;
+                    itemObj.name = itemToAdd.Title;
+
+                    break; //ABbrechen, nachdems gefunden wurde
+                }
+            }
+        }
+       
+    }//endeAddItem
+
+    bool CheckIfItemIsInInventory(Item item)
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+                 if (items[i].ID == item.ID)
+                {
+                    return true;
+                }
+         }
+           
+
+        
+        return false;
     }
 }
---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class ItemData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    //Speichern die Events, zb ich klicke auf ein Objekt und kann es bewegen - 
+    //wichtig, wir schieben das Item vor das parent objekt und EndDrag wieder zurück
+
+    public Item item;
+    public int amount;
+    public int slot;
+
+    private Inventory inv;
+    //private Transform originalParent;
+    private Vector2 offset;
+
+    void Start()
+    {
+        inv = GameObject.Find("Inventory").GetComponent<Inventory>();
+    }
+     public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (item != null)
+        {//--Wir haben ein item:
+            offset = eventData.position - new Vector2(this.transform.position.x, this.transform.position.y);
+            //originalParent = this.transform.parent; //Speicher die OriginalGröße des ParentPanelab
+            this.transform.SetParent(this.transform.parent.parent);
+            this.transform.position = eventData.position - offset;
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (item != null)
+        {//--Wir haben ein item:
+            this.transform.position = eventData.position -offset;
+        }
+    }
+
+   
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.SetParent(inv.slots[slot].transform);
+        this.transform.position = inv.slots[slot].transform.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+    }
+}
